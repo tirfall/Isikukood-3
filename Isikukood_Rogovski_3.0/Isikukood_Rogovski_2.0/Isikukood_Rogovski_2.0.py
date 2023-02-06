af08f510bae76c3e2aa4fb895521f88927a79f87
Aleksander Rogovski <61885744+tirfall@users.noreply.github.com>
2023-02-06 15:03:18 +0200

    Add files via upload

---
+++ Isikukood_Rogovski_3.0/Isikukood_Rogovski_2.0/Isikukood_Rogovski_2.0.py
@@ -0,0 +1,87 @@
+﻿from xmlrpc.client import DateTime
+from oma_moodul2 import *
+import pickle
+import os
+
+ikood=""
+ikoodid=[]
+arvud=[]
+adminpass = 8585
+
+storage_filename1 = "ikoodid"
+if not os.path.exists(storage_filename1): 
+    with open(storage_filename1, "wb") as f: 
+        pickle.dump(ikoodid, f)
+else: 
+    with open(storage_filename1, "rb") as f:
+        ikoodid = pickle.load(f)
+
+storage_filename2 = "arvud"
+if not os.path.exists(storage_filename2): 
+    with open(storage_filename2, "wb") as f: 
+        pickle.dump(arvud, f)
+else: 
+    with open(storage_filename2, "rb") as f:
+        arvud = pickle.load(f)
+
+while True:
+    choice=int(input("1 - vaata arvud \n2 - vaata ikoodid \n3 - isikukood \n4 - sorteeri arvud \n5 - sorteeri ikoodid \n6 - admin panel\n7 - stop programmi\n"))
+    if choice == 1:
+        print(arvud)
+    elif choice == 2:
+        print(ikoodid)
+    elif choice == 3:
+        ikood=input("Sisesta isikukood: ")
+        if pikkus(ikood)==False:
+            print("Liiga pikk või lühike isikukood")
+            arvud.append(ikood)
+        else:
+            s = esimine (ikood)
+            if s=="viga":
+                print("Esimene täht ei ole õige")
+                arvud.append(ikood)
+            else:
+                if sunnipaev(ikood)=="viga":
+                    print("2-7 tähed on valesti sisestatud")
+                    arvud.append(ikood)
+                else:
+                    lause(ikood)
+                    print(kontrollnr(ikood))
+                    ikoodid.append(ikood)
+    elif choice == 4:
+        arvud = arvud_sorted(arvud)
+        print(arvud)
+    elif choice == 5:
+        ikoodid = naised_mehed(ikoodid)
+        print(ikoodid)
+    elif choice == 6:
+        anspass = int(input("Enter password:"))
+        if anspass == adminpass:
+            while True:
+                adminchoice = int(input("Menu:\n1 - delete ikood from arvud\n2 - delete ikood from ikoodid\n3 - vaata arvud ja ikoodid\n4 - exit from admin menu\n"))
+                if adminchoice == 1:
+                    print(arvud)
+                    removechoice = input("Enter ikood to remove:")
+                    arvud.remove(removechoice)
+                elif adminchoice == 2:
+                    print(ikoodid)
+                    removechoice = input("Enter ikood to remove:")
+                    ikoodid.remove(removechoice)
+                elif adminchoice == 3:
+                    print(ikoodid)
+                    print(arvud)
+                elif adminchoice == 4:
+                    break
+                else:
+                    print("Vale number")
+                    ValueError
+    elif choice == 7:
+        print("Head aega!")
+        break
+
+with open(storage_filename1, "wb") as f:
+                pickle.dump(ikoodid, f)
+with open(storage_filename2, "wb") as f:
+                pickle.dump(arvud, f)
+
+
---
+++ Isikukood_Rogovski_3.0/Isikukood_Rogovski_2.0/oma_moodul2.py
@@ -0,0 +1,121 @@
+﻿
+from calendar import month
+from re import S
+
+
+def pikkus(ikood:str)->bool:
+    if len(ikood)==11 and ikood.isdigit():
+        flag=True
+    else:
+        flag=False
+    return flag
+
+def esimine(ikood:str)->str:
+    
+    ikood_list=list(map(int,ikood)) #[1,2,...]
+    if ikood_list[0] in [1,3,5]:
+        s="mees"
+    elif ikood_list[0] in [2,4,6]:
+        s="naine"
+    else:
+        s="viga"
+    return s
+
+def sunnipaev(ikood:str)->str:
+    spaev=0
+    ikood_list=list(ikood)
+    y=ikood_list[1]+ikood_list[2]
+    m=ikood_list[3]+ikood_list[4]
+    d=ikood_list[5]+ikood_list[6]
+    if int(d)>=1 and int(d)<=31 and int(m)>=1 and int(m)<=12:
+        if int(ikood_list[0]) in [1,2]:
+            yy="18"
+        elif int(ikood_list[0]) in [3,4]:
+            yy="19"
+        else:
+            yy="20"
+        spaev=(f"{d}.{m}.{yy}{y}")
+    else:
+        print("Viga")
+    return spaev
+ 
+
+def sunnikoht(ikood: str)->str:
+    ikood_list=list(ikood) 
+    t=int(ikood_list[7]+ikood_list[8]+ikood_list[9])
+    
+    if t>=1 and t<=10:
+        haigla="Kuressaare Haigla"
+    elif t>=11 and t<=19:
+        haigla="Taru Ülikooli NAistekliinik, Tartumaa, Tartu"
+    elif t>=20 and t<=220:
+        haigla="Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla"
+    elif t>=221 and t<=270:
+        haigla=" Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)"
+    elif t>=271 and t<=370:
+        haigla="Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
+    elif t>=371 and t<=420:
+        haigla="Narva Haigla"
+    elif t>=421 and t<=470:
+        haigla="Pärnu Haigla"
+    elif t>=471 and t<=490:
+        haigla="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
+    elif t>=491 and t<=520:
+        haigla="Järvamaa Haigla (Paide)"
+    elif t>=521 and t<=570:
+        haigla="Rakvere, Tapa haigla"
+    elif t>=571 and t<=600:
+        haigla="Valga Haigla"
+    elif t>=601 and t<=650:
+        haigla="Viljandi Haigla"
+    elif t>=651 and t<=700:
+        haigla<="Lõuna-Eesti Haigla (Võru), Põlva Haigla"
+    else:
+        haigla="Välismaal"
+    return haigla
+
+
+
+def lause(ikood)->str:
+    print(f"See on {esimine(ikood)} ta on sündinud {sunnipaev(ikood)}, tema sünnikoht on {sunnikoht(ikood)}")
+
+
+
+def kontrollnr(ikood:str)->int:
+    astme1=[1,2,3,4,5,6,7,8,9,1]
+    astme2=[3,4,5,6,7,8,9,1,2,3]
+    ik_list=list(ikood)
+    ik_list=list(map(int,ik_list))
+    summa=0
+    for i in range (0,10,1):
+        summa+=ik_list[i]+astme1[i]
+    s=(summa//11)*11
+    jaak=summa-s
+    if jaak==int(ik_list[10]):
+        return jaak
+    elif jaak==10:
+        return 0
+    else:
+        for i in range(0,10,1):
+            summa+=ik_list[i]+astme2[i]
+        s=(summa//11)*11
+        jaak=summa-s
+        return jaak
+
+def naised_mehed(ikoodid:list)->list:
+    naised = []
+    mehed = []
+    for kood in ikoodid:
+        kood_=list(kood)
+        if int(kood_[0])%2==0:
+            naised.append(kood)
+        else:
+            mehed.append(kood)
+    naised.extend(mehed)
+    ikoodid=naised
+    return naised
+
+def arvud_sorted(arvud:list)->list:
+    arvud=list(map(int,arvud))
+    arvud.sort()
+    return arvud
